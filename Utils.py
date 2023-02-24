a10c9d502347e4581c6dbb0f0882e17aa6c92879
Luca Lin <agahamsc@live.com>
2023-02-24 21:24:43 +0100

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -381,20 +381,7 @@ def count_parameters(model):
     num_params = sum([np.prod(p.size()) for p in model_parameters])
     print("Total parameters =", num_params)
     return num_params
-class WarmupLinearSchedule(LambdaLR):
-    """ Linear warmup and then linear decay.
-        Linearly increases learning rate from 0 to 1 over `warmup_steps` training steps.
-        Linearly decreases learning rate from 1. to 0. over remaining `t_total - warmup_steps` steps.
-    """
-    def __init__(self, optimizer, warmup_steps, t_total, last_epoch=-1):
-        self.warmup_steps = warmup_steps
-        self.t_total = t_total
-        super(WarmupLinearSchedule, self).__init__(optimizer, self.lr_lambda, last_epoch=last_epoch)
 
-    def lr_lambda(self, step):
-        if step < self.warmup_steps:
-            return float(step) / float(max(1, self.warmup_steps))
-        return max(0.0, float(self.t_total - step) / float(max(1.0, self.t_total - self.warmup_steps)))
 def log_eval_metrics(model, train_losses, train_accuracies, val_dataloader, val_hard_dataloader, loss_fn, optimizer_bert, optimizer_other, device, wandb):
     val_loss, val_accuracy = eval_model(model, val_dataloader, loss_fn, device)
     val_loss_hard, val_accuracy_hard = eval_model(model, val_hard_dataloader, loss_fn, device)
