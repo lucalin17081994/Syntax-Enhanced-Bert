1ea1079aed95a35706ea82c9ab70ce040955c33a
Luca Lin <agahamsc@live.com>
2023-03-07 16:39:39 +0100

    Update sbatch.sh

--- sbatch.sh
+++ sbatch.sh
@@ -5,4 +5,11 @@
 #SBATCH --partition=gpu
 #SBATCH --time=08:00:00
 
+module load 2021
+module load 2022
+module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
+module load Python/3.10.4-GCCcore-11.3.0
+pip install scikit-learn
+pip install pandas
+
 python Main.py
