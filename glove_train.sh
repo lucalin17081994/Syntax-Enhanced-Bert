b0085c3638dfa4c794954432f1d84822fdd7377c
Luca Lin <agahamsc@live.com>
2023-04-21 12:20:16 +0200

    Create glove_train.sh

---
+++ glove_train.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus=1
+#SBATCH --partition=gpu
+#SBATCH --time=07:00:00
+
+ls
+module load 2021
+module load 2022
+module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
+module load Python/3.10.4-GCCcore-11.3.0
+pip install --upgrade scikit-learn
+pip install --upgrade pandas
+pip install wandb
+
+
+
+python Glove.py
