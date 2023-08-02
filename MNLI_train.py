31e1fd3426bf64a61998320dce328583aa75ed89
Luca Lin <agahamsc@live.com>
2023-08-02 12:33:42 +0200

    Update MNLI_train.py

--- MNLI_train.py
+++ MNLI_train.py
@@ -203,7 +203,7 @@ else:
     optimizer_other=None
     
 import wandb
-wandb.login(key = 'a72edb442b6177a7198f045dee1e6b7c4de8f7a3')
+wandb.login(key = '...')
 run = wandb.init(project=run_name)
 model_name = model_name
 wandb.config.update({
